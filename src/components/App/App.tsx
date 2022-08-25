c06afed6216a6896828ca4ed4c46c3c719f55394
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-08-25 22:10:24 +0200

    project initial  folders structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import "../../assets/styles/App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+};
+
+export default App;
